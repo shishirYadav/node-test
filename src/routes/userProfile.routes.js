c97de7046e868a5a9f3688b28650b6c80f3e5216
Shishir yadav <shishir569@gmail.com>
2022-11-26 06:04:39 -0800

    Add files via upload

--- src/routes/userProfile.routes.js
+++ src/routes/userProfile.routes.js
@@ -7,7 +7,7 @@ const ProfileController = require('../controllers/userProfile.controller')
 route.delete('/deleteUser',  ProfileController.deleteUser)
 route.put('/updateUser',  ProfileController.updateUser)
 route.post('/createUser', ProfileController.createUser)
-route.post('/fetchAllUser', ProfileController.fetchAllUser)
+route.get('/fetchAllUser', ProfileController.fetchAllUser)
 
 
 
